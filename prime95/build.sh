a8db0e4e22e2965ae6e9f8c1c418ca194adbaa9a
James MacMahon <jwm@operand.ca>
2022-05-25 13:10:17 -0400

    update build instructions

--- prime95/build.sh
+++ prime95/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PATH="${PWD}/../UASM/GccUnixR/:${PATH}"
-uasm64 \
+uasm \
     -c -DX86_64 -DLINUX64 -DARCH=FMA3 \
     -I../gwnum -win64 -archAVX -elf64 \
     -Fo ../linux64/factor64.o factor64.asm
